00f612bdf82d0e8777472737cb2aa67e792da3bc
hayong <hayong@218.38.137.28>
2018-01-06 21:06:20 +0900

    last

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -44,12 +44,15 @@ public class Calc {
 		System.out.println("4. Quit");
 		System.out.println("\nchoose the number");
 	}
+	
 	public static int add(int number1, int number2){
 		return number1 + number2;
 	}
+	
 	public static int subtract(int number1, int number2){
 		return number1 - number2;
 	}
+	
 	public static int multiply(int number1, int number2){
 		return number1 * number2;
 	}
