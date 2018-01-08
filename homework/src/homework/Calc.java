aeeab863e5cd786c4547d8e6c0c9d429c2fe1c0e
hayong <hayong@192.168.0.94>
2018-01-08 20:18:51 +0900

    erase some method for adding branch (add, subtract, multiply)

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -45,15 +45,5 @@ public class Calc {
 		System.out.println("\nchoose the number");
 	}
 	
-	public static int add(int number1, int number2){
-		return number1 + number2;
-	}
 	
-	public static int subtract(int number1, int number2){
-		return number1 - number2;
-	}
-	
-	public static int multiply(int number1, int number2){
-		return number1 * number2;
-	}
 }
