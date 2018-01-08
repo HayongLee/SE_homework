134467f04e5fe0d371a4834e6aed0c9b45c02c18
hayong <hayong@localhost>
2018-01-09 00:40:30 +0900

    master

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -20,8 +20,6 @@ public class Calc {
 				result = add(number1, number2);
 			else if(select == 2)
 				result = subtract(number1, number2);
-			else
-				result = multiply(number1, number2);
 			
 			System.out.println("x: " + number1);
 			System.out.println("y: " + number2);
@@ -53,7 +51,4 @@ public class Calc {
 		return number1 - number2;
 	}
 	
-	public static int multiply(int number1, int number2){
-		return number1 * number2;
-	}
 }
