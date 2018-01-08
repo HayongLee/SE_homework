cba44aa352b971b4f573b9184c0841f487d7e664
hayong <hayong@localhost>
2018-01-09 00:27:27 +0900

    <create subtract method>

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -45,6 +45,7 @@ public class Calc {
 	public static int add(int number1, int number2){
 		return number1 + number2;
 	}
+	
 	public static int subtract(int number1, int number2){
 		return number1 - number2;
 	}
