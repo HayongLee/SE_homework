0e3cc553d4284354d4deca3a05cded8d56582aa8
hayong <hayong@localhost>
2018-01-09 00:36:18 +0900

    <create multiply method>

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -48,9 +48,11 @@ public class Calc {
 	public static int add(int number1, int number2){
 		return number1 + number2;
 	}
+	
 	public static int subtract(int number1, int number2){
 		return number1 - number2;
 	}
+	
 	public static int multiply(int number1, int number2){
 		return number1 * number2;
 	}
