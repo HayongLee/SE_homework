7d562efa0c3c833c076b7a34cdaaa83372fbe771
hayong <hayong@192.168.25.198>
2018-01-12 13:23:06 +0900

    master

--- homework/src/homework/Calc.java
+++ homework/src/homework/Calc.java
@@ -2,13 +2,14 @@ package homework;
 
 import java.util.Scanner;
 
-public class Calc {
+public class Calc
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner in = new Scanner(System.in);
 		int result = 0;
 		printManual();
-		
 		int select = in.nextInt();
 		
 		while(select != 4)
@@ -28,6 +29,7 @@ public class Calc {
 			System.out.println("y: " + number2);
 			break;
 		}
+		
 		if(select == 4)
 		{
 			System.exit(0);
@@ -36,23 +38,30 @@ public class Calc {
 		System.out.println("Result: " + result);
 		
 	}
-	public static void printManual(){
+	
+	public static void printManual()
+	{
 		System.out.println("[ID: 1771504]");
-		System.out.println("[Name: 이하용]\n");
-		
+		System.out.println("[Name: 이하용]\n");	
 		System.out.println("1. Add two numbers");
 		System.out.println("2. Subtract two numbers");
 		System.out.println("3. Multiply two numbers");
 		System.out.println("4. Quit");
 		System.out.println("\nchoose the number");
 	}
-	public static int add(int number1, int number2){
+	
+	public static int add(int number1, int number2)
+	{
 		return number1 + number2;
 	}
-	public static int subtract(int number1, int number2){
+	
+	public static int subtract(int number1, int number2)
+	{
 		return number1 - number2;
 	}
-	public static int multiply(int number1, int number2){
+	
+	public static int multiply(int number1, int number2)
+	{
 		return number1 * number2;
 	}
 }
